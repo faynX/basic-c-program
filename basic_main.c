15beee7b6cc7d52c6d4b56a391a7af1e8dc52f46
faynX <92753284+faynX@users.noreply.github.com>
2022-09-29 07:49:32 +0200

    Added loop
    Added loop

--- basic_main.c
+++ basic_main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello, World!");
+   printf("Hello, World !\n");
+   for(int i = 0;i<10;i++) {
+     printf("Item boucle %d\n",i);
+   }
    return 0;
 }
